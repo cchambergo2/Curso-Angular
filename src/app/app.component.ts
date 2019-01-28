fd19be4b8557c3e22b20a0762f7b8b6264a07626
cchambergo2 <cchambergo2@hotmail.com>
2019-01-27 19:09:26 -0500

    ngStyle-ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ export class AppComponent {
   title = 'platzisquare';
   lugares: any = [
     { active: true, nombre: 'Floreria' },
-    { active: true, nombre: 'Donuts' },
+    { active: false, nombre: 'Donuts' },
     { active: true, nombre: 'Veterinaria' },
-    { active: true, nombre: 'sushi' },
+    { active: false, nombre: 'sushi' },
     { active: true, nombre: 'zapateria' },
   ];
   lat:number = -12.024911;
