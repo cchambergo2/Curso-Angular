5e127d39466ffc5b2db6bbd3a77771d9d7db3bcb
cchambergo2 <cchambergo2@hotmail.com>
2019-01-27 19:40:15 -0500

    ngSwitch y ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'platzisquare';
   lugares: any = [
-    { active: true, nombre: 'Floreria' },
-    { active: false, nombre: 'Donuts' },
-    { active: true, nombre: 'Veterinaria' },
-    { active: false, nombre: 'sushi' },
-    { active: true, nombre: 'zapateria' },
+    {cercania: 1, distancia: 1, active: false, nombre: 'Donuts' },
+    {cercania: 1, distancia: 1.80, active: true, nombre: 'Veterinaria' },
+    {cercania: 2, distancia: 5, active: false, nombre: 'sushi' },
+    {cercania: 3, distancia: 10, active: true, nombre: 'Floreria' },
+    {cercania: 3, distancia: 35, active: true, nombre: 'zapateria' },
   ];
   lat:number = -12.024911;
   lng:number =  -77.066682;
