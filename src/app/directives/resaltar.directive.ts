0df9f91f98561e008dbf94254fb14ca9be3e9d02
cchambergo2 <cchambergo2@hotmail.com>
2019-01-30 22:22:59 -0500

    Host Binders editar el dom desde una directiva

--- src/app/directives/resaltar.directive.ts
+++ src/app/directives/resaltar.directive.ts
@@ -5,7 +5,7 @@ import { Directive, OnInit, ElementRef, Renderer2, Input } from "@angular/core";
 })
 export class ResaltarDirective implements OnInit{
     constructor(private elRef: ElementRef, private renderer: Renderer2){}
-    @Input('resaltar') plan : string = '';
+    @Input('resaltar') plan = '';
     ngOnInit(){
         if(this.plan === 'pagado'){
             this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow');
