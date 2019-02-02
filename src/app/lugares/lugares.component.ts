b2d0938f22719d1a3671515a2c62563c7e377dc4
cchambergo2 <cchambergo2@hotmail.com>
2019-02-01 21:22:43 -0500

    Parámetros en Rutas

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -7,11 +7,11 @@ import { Component } from '@angular/core';
 export class LugaresComponent {
   title = 'platzisquare';
   lugares: any = [
-    { plan: 'pagado', cercania: 1, distancia: 1, active: false, nombre: 'Donuts' },
-    { plan: 'gratuito', cercania: 1, distancia: 1.80, active: true, nombre: 'Veterinaria' },
-    { plan: 'pagado', cercania: 2, distancia: 5, active: false, nombre: 'sushi' },
-    { plan: 'gratuito', cercania: 3, distancia: 10, active: true, nombre: 'Floreria' },
-    { plan: 'gratuito', cercania: 3, distancia: 35, active: true, nombre: 'zapateria' },
+    {id: 1, plan: 'pagado', cercania: 1, distancia: 1, active: false, nombre: 'Donuts' },
+    {id: 2, plan: 'gratuito', cercania: 1, distancia: 1.80, active: true, nombre: 'Veterinaria' },
+    {id: 3, plan: 'pagado', cercania: 2, distancia: 5, active: false, nombre: 'sushi' },
+    {id: 4, plan: 'gratuito', cercania: 3, distancia: 10, active: true, nombre: 'Floreria' },
+    {id: 5, plan: 'gratuito', cercania: 3, distancia: 35, active: true, nombre: 'zapateria' },
   ];
   lat = -12.024911;
   lng = -77.066682;
